e13927f41cd82c465cbae0a20ecd0eb52faabdba
Voxuel <Leo.Fridh@hotmail.com>
2022-10-20 13:34:57 +0200

    Created methods for calculating circumference and volume of circles

--- CirclePorgram/Circle.cs
+++ CirclePorgram/Circle.cs
@@ -7,13 +7,34 @@ namespace CirclePorgram
     class Circle
     {
         private float Area;
+        private float Volume;
+        private float Circumference;
+        private float Radius;
+        private float Height;
+        private float Pi = 3.141f;
         public Circle(float radius)
         {
-            this.Area = radius * radius * 3.141f;
+            this.Radius = radius;
+        }
+        public Circle(float radius, float height)
+        {
+            this.Radius = radius;
+            this.Height = height;
         }
         public float GetArea()
         {
+            Area = Radius * Radius * 3.141f;
             return Area;
+        }
+        public float GetVolume()
+        {
+            Volume = Pi * (Radius * Radius) * Height;
+            return Volume;
+        }
+        public float GetCircumference()
+        {
+            Circumference = (Radius * 2) * Pi;
+            return Circumference;
         }
     }
 }
--- CirclePorgram/Program.cs
+++ CirclePorgram/Program.cs
@@ -10,6 +10,9 @@ namespace CirclePorgram
             Console.WriteLine(circle1.GetArea());
             var circle2 = new Circle(6);
             Console.WriteLine(circle2.GetArea());
+            var circle3 = new Circle(5, 10);
+            Console.WriteLine(circle3.GetVolume());
+            Console.WriteLine(circle1.GetCircumference());
         }
     }
 }
